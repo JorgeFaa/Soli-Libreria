613f42f9c0cd0b733f937bc7df724c2060b02288
Alonso Gutiérrez <agutierrez109@alumnos.uaq.mx>
2025-08-29 19:53:52 -0600

    Fix: added Auxiliary tables

--- SoliApi/src/main/resources/schema.sql
+++ SoliApi/src/main/resources/schema.sql
@@ -3,32 +3,46 @@ CREATE database Soli_DB;
 create table Soli_DB.public.Texts(
     textID serial not null primary key,
     textTitle varchar(250) not null,
-    publishedDate date,
-    textType varchar(50) not null ,
-    genre varchar(20) not null
+    publishedDate date
 );
 
 create table Soli_DB.public.Authors(
     authorID serial not null primary key,
     authorName varchar(50) not null ,
     AuthorMiddleName varchar(50),
-    authorLastName varchar(100),
-    country varchar(50) not null
+    authorLastName varchar(100)
 );
 create table  Soli_DB.public.Editorials(
     editorialID serial not null primary key,
-    companyName varchar(250) not null ,
-    country varchar(60) not null
+    companyName varchar(250) not null
 );
 
-Create table Soli_DB.public.users(
+Create table Soli_DB.public.Users(
     userID serial not null primary key,
     firstName varchar(50) not null,
     lastName varchar(50) not null ,
-    userEmail varchar(100) not null ,
-    userRole varchar(15) not null ,
     activeMember boolean not null,
     genrePreference varchar(256)
+);
+
+Create table Soli_DB.public.Country(
+    countryID serial not null primary key,
+    countryName varchar
+);
+
+Create Table Soli_DB.public.Genres(
+    genreID serial not null primary key,
+    genreName varchar
+);
+
+Create table Soli_DB.public.textType(
+    typeID serial not null primary key,
+    textType varchar
+);
+
+Create table Soli_DB.public.Roles(
+    roleID serial not null primary key,
+    role varchar
 );
 
 alter table Soli_DB.public.Texts add column
@@ -36,3 +50,18 @@ alter table Soli_DB.public.Texts add column
 
 alter table Soli_DB.public.Texts add column
     editorialID int references Editorials(editorialID);
+
+alter table Soli_DB.public.Texts add column
+    genreID int references Genres(genreID);
+
+alter table Soli_DB.public.Texts add column
+    typeID int references textType(typeID);
+
+alter table Soli_DB.public.Authors add column
+    countryID int references Country(countryID);
+
+alter table Soli_DB.public.Editorials add column
+    countryID int references Country(countryID);
+
+alter table Soli_DB.public.Users add column
+    roleID int references Roles(roleID);
